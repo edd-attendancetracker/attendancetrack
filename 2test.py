8401a07fd80fe25f4bd5472bbe3063babcb97483
Juwanz <haozewang1279@gmail.com>
2023-08-25 11:43:26 -0500

    second commit

--- 2test.py
+++ 2test.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+
+import numpy as np
 import cv2
 from deepface import DeepFace
 from time import sleep
@@ -7,6 +9,8 @@ from time import sleep
 roster = {
         "Hagan Riethmiller" : '/home/haozewang/Pictures/Webcam/Hagan_Riethmiller.jpg'
         }
+mugshots = []
+
 #takes a picture every quarter second and saves it
 def collect_pictures():
     # Set up the camera
@@ -26,12 +30,24 @@ def collect_pictures():
             raise Exception("Could not capture frame")
         # Save the captured frame to an image file
         cv2.imwrite('image'+str(n)+'.jpg', frame)
-        n+=1;
+        mugshots.append('image'+str(n)+'.jpg')
+        n+=1
+        print(mugshots)
     # Release the camera and destroy all windows
     cap.release()
     cv2.destroyAllWindows()
 
 #processes the location of the face in each picture taken
+import numpy as np
+
+def process_pictures():
+    for i in range(len(mugshots)):
+        detected_faces = DeepFace.extract_faces(mugshots[i])
+        for j, face_info in enumerate(detected_faces):
+            face = face_info['face']
+            # Convert the image data to 8-bit format
+            face = np.uint8(face * 255)
+            cv2.imwrite('cropped_face{}_{}.jpg'.format(i, j), face)
 
 collect_pictures()
-
+process_pictures()

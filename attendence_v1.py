1d401fc44a5e1e6ac35e032aae6aa7df156bc210
Juwanz <haozewang1279@gmail.com>
2023-08-29 12:31:38 -0500

    for presentation

--- attendence_v1.py
+++ attendence_v1.py
@@ -7,8 +7,14 @@ from time import sleep
 
 #list of all people's names and their photo directories
 roster = {
-        "Hagan Riethmiller" : '/home/haozewang/Facial_Recognition/attendancetrack/roster/Hagan_Riethmiller.png',
-        "Haoze Wang" : '/home/haozewang/Facial_Recognition/attendancetrack/roster/Haoze_Wang.png'
+        "Hagan Riethmiller" : '/home/haozewang/Facial_Recognition/attendancetrack/roster/Hagan_Riethmiller.jpg',
+        "Haoze Wang" : '/home/haozewang/Facial_Recognition/attendancetrack/roster/Haoze_Wang.png',
+        "Jadon Lee" : '/home/haozewang/Facial_Recognition/attendancetrack/roster/Jadon Lee.jpg',
+        "Jeremy Suen" : '/home/haozewang/Facial_Recognition/attendancetrack/roster/Jeremy_Suen.jpg',
+        #"Keshav Sreekantham" : '/home/haozewang/Facial_Recognition/attendancetrack/roster/Keshav_Sreekantham.jpg', 
+        #"Nikhil Modak" : '/home/haozewang/Facial_Recognition/attendancetrack/roster/Nikhil_Modak.jpg',
+        "Sofia Davila Ramirez" : '/home/haozewang/Facial_Recognition/attendancetrack/roster/Sofia_Davilaramirez.jpg',
+        "Mannat Ahluwalia" : '/home/haozewang/Facial_Recognition/attendancetrack/roster/Mannat_Ahluwalia.jpg'
         }
 mugshots = []
 cropshots = []
@@ -19,7 +25,18 @@ def collect_pictures():
     n=0
     # Capture a frame from the camera every 0.25 seconds
     #this will run for 1 seconds
-    while n<4:
+    while n<3:
+        print('taking picture in 5')
+        sleep(1)
+        print('taking picture in 4')
+        sleep(1)
+        print('taking picture in 3')
+        sleep(1)
+        print('taking picture in 2')
+        sleep(1)
+        print('taking picture in 1')
+        sleep(1)
+        print('smile :)')
          # Set up the camera
         cap = cv2.VideoCapture(0)
         # Check if the camera was opened successfully
@@ -33,8 +50,7 @@ def collect_pictures():
         cv2.imwrite('/home/haozewang/Facial_Recognition/attendancetrack/webcam_pictures/image'+str(n)+'.jpg', frame)
         mugshots.append('/home/haozewang/Facial_Recognition/attendancetrack/webcam_pictures/image'+str(n)+'.jpg')
         n+=1
-        print(mugshots)
-        sleep(0.1)
+        #print(mugshots)      
         # Release the camera and destroy all windows
         cap.release()
         cv2.destroyAllWindows()
@@ -49,15 +65,15 @@ def process_pictures():
             face = np.uint8(face * 255)
             cv2.imwrite('/home/haozewang/Facial_Recognition/attendancetrack/cropped_pictures/cropped_face{}_{}.jpg'.format(i, j), face)
             cropshots.append('/home/haozewang/Facial_Recognition/attendancetrack/cropped_pictures/cropped_face{}_{}.jpg'.format(i,j))
-            print(cropshots)
+            #print(cropshots)
 #recursively compares each mugshot to the reference photos to determine who are in the mugshots
 def compare_pictures():
     for i in range(len(roster)):
         print(list(roster.values())[i])
-        result = DeepFace.find(img_path = str(list(roster.values())[i]), db_path = '/home/haozewang/Facial_Recognition/attendancetrack/cropped_pictures/', enforce_detection = False)[0]
+        result = DeepFace.find(img_path = str(list(roster.values())[i]), db_path = '/home/haozewang/Facial_Recognition/attendancetrack/cropped_pictures/', enforce_detection=False)[0]
         print(list(roster.keys())[i]+':')
         print(result)
 
 collect_pictures()
-process_pictures()
+#process_pictures()
 compare_pictures()
